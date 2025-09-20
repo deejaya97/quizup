f5c58e260cace0d264cc9f3e514760d06095c359
deejaya97 <flyHigh@700>
2025-09-20 14:43:36 -0400

    DJ-0100

---
+++ src/assets/data.js
@@ -0,0 +1,92 @@
+export const data = [
+    {
+        id: 1,
+        question: "What does HTTP stand for?",
+        option1: "HyperText Transfer Protocol",
+        option2: "HighText Transfer Protocol",
+        option3: "HyperText Transmission Process",
+        option4: "HyperTransfer Text Protocol",
+        ans: 1
+    },
+    {
+        id: 2,
+        question: "Which company developed the Python programming language?",
+        option1: "Google",
+        option2: "Microsoft",
+        option3: "CWI (Centrum Wiskunde & Informatica)",
+        option4: "IBM",
+        ans: 3
+    },
+    {
+        id: 3,
+        question: "What does CSS stand for?",
+        option1: "Color Style Sheets",
+        option2: "Creative Style System",
+        option3: "Cascading Style Sheets",
+        option4: "Computer Styled Sections",
+        ans: 3
+    },
+    {
+        id: 4,
+        question: "Which is NOT a programming language?",
+        option1: "Python",
+        option2: "Ruby",
+        option3: "HTML",
+        option4: "Java",
+        ans: 3
+    },
+    {
+        id: 5,
+        question: "What year was JavaScript created?",
+        option1: "1993",
+        option2: "1995",
+        option3: "1998",
+        option4: "2000",
+        ans: 2
+    },
+    {
+        id: 6,
+        question: "Which data structure uses FIFO (First In First Out)?",
+        option1: "Stack",
+        option2: "Queue",
+        option3: "Tree",
+        option4: "Graph",
+        ans: 2
+    },
+    {
+        id: 7,
+        question: "What symbol is used to denote an ID in CSS?",
+        option1: ".",
+        option2: "#",
+        option3: "@",
+        option4: "$",
+        ans: 2
+    },
+    {
+        id: 8,
+        question: "Which company developed the React library?",
+        option1: "Google",
+        option2: "Microsoft",
+        option3: "Facebook",
+        option4: "Amazon",
+        ans: 3
+    },
+    {
+        id: 9,
+        question: "In Python, what is the output of len('Hello World')?",
+        option1: "10",
+        option2: "11",
+        option3: "12",
+        option4: "None of the above",
+        ans: 3
+    },
+    {
+        id: 10,
+        question: "Which of these is a NoSQL database?",
+        option1: "MySQL",
+        option2: "PostgreSQL",
+        option3: "MongoDB",
+        option4: "Oracle",
+        ans: 3
+    }
+]
